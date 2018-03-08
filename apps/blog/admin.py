89307345cad2ea1a104ddfe5784d3476374547d3
eason <wangyisheng2014@hotmail.com>
2018-03-08 11:16:17 +0800

    注册django_summernote模型

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Article, Category, Tag
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 
-admin.site.register(Article)
 admin.site.register(Category)
 admin.site.register(Tag)
+
+
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)  # 给content字段添加富文本
+
+
+admin.site.register(Article, PostAdmin)
