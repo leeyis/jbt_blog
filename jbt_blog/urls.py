35f82c6b246f3c317fa17a0641869c1f38502e73
eason <wangyisheng2014@hotmail.com>
2018-03-08 11:18:09 +0800

    添加summernote路由

--- jbt_blog/urls.py
+++ jbt_blog/urls.py
@@ -16,10 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from apps.blog import views
+from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('home/', views.home, name='home'),
     path('articles/<int:id>/', views.detail, name='detail'),
+    path('summernote/', include('django_summernote.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
