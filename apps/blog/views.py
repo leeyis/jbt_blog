d370f9b1c637d9d0b2226972f0da2c39fdcc0c18
eason <wangyisheng2014@hotmail.com>
2018-03-11 22:50:06 +0800

    增加分类搜索和标签搜索功能

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,9 +1,11 @@
-
 from django.shortcuts import render
-from apps.blog.models import Article
+from apps.blog.models import Article, Category, Tag
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import Http404
 from django.conf import settings
+
+categories = Category.objects.all()  # 获取全部的分类对象
+tags = Tag.objects.all()  # 获取全部的标签对象
 
 
 # Create your views here.
@@ -17,15 +19,43 @@ def home(request):  # 主页
         post_list = paginator.page(1)
     except EmptyPage:
         post_list = paginator.page(paginator.num_pages)
-    return render(request, 'home.html', {'post_list': post_list})
+    return render(request, 'home.html', {'post_list': post_list, 'category_list': categories})
 
 
 def detail(request, id):
     try:
-
         post = Article.objects.get(id=str(id))
         post.viewed()  # 更新浏览次数
         tags = post.tags.all()
     except Article.DoesNotExist:
         raise Http404
-    return render(request, 'post.html', {'post': post, 'tags': tags})
+    return render(request, 'post.html', {'post': post, 'tags': tags, 'category_list': categories})
+
+
+def search_category(request, id):
+    posts = Article.objects.filter(category_id=str(id))
+    category = categories.get(id=str(id))
+    paginator = Paginator(posts, settings.PAGE_NUM)  # 每页显示数量
+    try:
+        page = request.GET.get('page')  # 获取URL中page参数的值
+        post_list = paginator.page(page)
+    except PageNotAnInteger:
+        post_list = paginator.page(1)
+    except EmptyPage:
+        post_list = paginator.page(paginator.num_pages)
+    return render(request, 'category.html', {'post_list': post_list, 'category_list': categories, 'category': category})
+
+
+def search_tag(request, tag):
+    posts = Article.objects.filter(tags__name__contains=tag)
+    paginator = Paginator(posts, settings.PAGE_NUM)  # 每页显示数量
+    try:
+        page = request.GET.get('page')  # 获取URL中page参数的值
+        post_list = paginator.page(page)
+    except Article.DoesNotExist:
+        raise Http404
+    except PageNotAnInteger:
+        post_list = paginator.page(1)
+    except EmptyPage:
+        post_list = paginator.page(paginator.num_pages)
+    return render(request, 'tag.html', {'post_list': post_list, 'category_list': categories, 'tag': tag})
