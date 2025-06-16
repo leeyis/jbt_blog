0f18d7b578716e05c34284ad613cd4908bf5764f
eason <wangyisheng2014@hotmail.com>
2025-06-16 13:48:28 +0800

    修复Markdown编辑器图片无法预览的问题

--- jbt_blog/urls.py
+++ jbt_blog/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from apps.blog import views
 from django.conf.urls import include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('manage/', admin.site.urls),
@@ -31,4 +31,5 @@ urlpatterns = [
     path('archives/<str:year>/<str:month>', views.archives, name='archives'),
     path('api/tagcloud/', views.tag_cloud_json, name='tag_cloud_json'),  # 标签云JSON API
     path('mdeditor/', include('mdeditor.urls')),  # 替换 summernote
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
